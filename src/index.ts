a8f66f0f0042f764f43359db09ae1c225cdb2d27
Gabe <gabrielgrechuk@outlook.com>
2024-12-31 01:35:01 -0300

    #3 - Export everithing in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export * from './gsr';
 export * from './pixel';
 export * from './tools';
+export * from './scene.2d';
+export * from './geometry/2d/mesh.2d';
+export * from './geometry/2d/point.2d';
+export * from './geometry/2d/rectangle';
